53d76e1ee5e50ac93f1aa2bf0654c9fbcc285c48
guzz <dekmis@hotmail.com>
2024-06-15 01:25:02 -0300

    refactor: new ICustomer interface

---
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -0,0 +1,13 @@
+import { ICreateCustomer } from '@modules/customers/infra/typeorm/entities/ICreateCustomer';
+import { ICustomer } from '../models/iCustomer';
+
+export interface ICustomersRepository {
+  findByName(name: string): Promise<ICustomer | undefined>;
+
+  findById(id: string): Promise<ICustomer | undefined>;
+
+  findByEmail(email: string): Promise<ICustomer | undefined>;
+
+  //create(data: ICreateCustomer): Promise<ICustomer>;
+  //save(customer: ICustomer): Promise<ICustomer>;
+}
