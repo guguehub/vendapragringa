1b4202b521611ede6b4b8f6ec4f1419bf07436cb
guzz <dekmis@hotmail.com>
2024-07-17 04:08:44 -0300

    (feat: jest test), fix: TS-JEST VERSION DOWNGRADED

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,8 @@
+describe('CreateCustomer', () => {
+  it('should be able to create a new customer', () => {
+    expect(1).toBe(1);
+  });
+  it('should not be able to create customer if email already taken', () => {
+    expect(1).toBe(1);
+  });
+});
