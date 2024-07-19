02dbec6c90690cb765adeb202176a11db7ae2f60
guzz <dekmis@hotmail.com>
2024-07-19 12:27:24 -0300

    feat: (JEST test on Create customer rep + mock fake rep)

---
+++ src/modules/customers/domain/repositories/fakes/FakeCustomersRepository.ts
@@ -0,0 +1,48 @@
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
+import { ICreateCustomer } from '../../../domain/models/ICreateCustomer';
+import { v4 as uuidv4 } from 'uuid';
+import { ICustomer } from '../../models/iCustomer';
+
+class FakeCustomersRepository
+  implements Omit<ICustomersRepository, 'remove' | 'findAll'>
+{
+  private customers: Customer[] = [];
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = new Customer();
+
+    customer.id = uuidv4();
+    customer.name = name;
+    customer.email = email;
+
+    this.customers.push(customer);
+    return customer;
+  }
+
+  public async findByName(name: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.name === name);
+    return customer;
+  }
+  public async findById(id: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.id === id);
+    return customer;
+  }
+
+  public async findByEmail(email: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.email === email);
+    return customer;
+  }
+  public async save(customer: Customer): Promise<Customer> {
+    Object.assign(this.customers, customer);
+    return customer;
+  }
+  remove(customer: ICustomer): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findAll(): Promise<ICustomer[] | undefined> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export default FakeCustomersRepository;
--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,8 +1,20 @@
+import 'reflect-metadata';
+import CreateCustomerService from './CreateCustomerService';
+import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
+
 describe('CreateCustomer', () => {
-  it('should be able to create a new customer', () => {
-    expect(1).toBe(1);
+  it('should be able to create a new customer', async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+    const customer = await createCustomer.execute({
+      name: 'John Silva',
+      email: 'teste@tester',
+    });
+    expect(customer).toHaveProperty('id');
   });
-  it('should not be able to create customer if email already taken', () => {
+
+  it('should NOT be able to create customer if email already taken', () => {
     expect(1).toBe(1);
   });
 });
